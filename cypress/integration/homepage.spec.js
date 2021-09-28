f4340f0b394c650e548f9cab582e7d1f9cb8cf99
zebasAL <zebastian_al@hotmail.com>
2021-09-27 19:03:00 -0700

    cypress homepage file fixed

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -17,8 +17,8 @@ describe('This will try every switch path elements', () => {
     cy.get('[data-cy="product-info-price"]').should('be.visible');
     cy.get('[data-cy="product-info-description"]').should('be.visible');
 
-    cy.get('[data-cy="quantity-input"]').select(1)
-    cy.get('[data-cy="quantity-input"]').type(21)
+    cy.get('[data-cy="quantity-input"]').select(1);
+    cy.get('[data-cy="quantity-input"]').type(21);
 
     cy.get('[data-cy="quantity-sum-button"]').click();
     cy.get('[data-cy="quantity-sum-button"]').click();
